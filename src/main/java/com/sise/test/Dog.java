825571e90a32b2f64edff3bfa798b602906712a5
wgx <wgx@qq.com>
2019-08-21 17:24:08 +0800

    edit Dog

--- src/main/java/com/sise/test/Dog.java
+++ src/main/java/com/sise/test/Dog.java
@@ -1,5 +1,34 @@
 package com.sise.test;
 
 public class Dog {
+	private String name;
+	
 
+	public Dog() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Dog(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + "]";
+	}
+	
 }
